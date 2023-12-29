cf9068dc63c122c2cfcef75a4efe5cdb85b9eb4a
Charity K <charity.kithaka@simplexi.io>
2023-12-29 04:38:18 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A project that uses unsplash images api and react query. 
+
+#### Getting started
+
+- npm install
+- npm run dev
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ThemeToggle from "./components/ThemeToggle";
+import SearchForm from "./components/SearchForm";
+import Gallery from "./components/Gallery";
+const App = () => {
+  return (
+    <>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </>
+  );
+};
+export default App;
